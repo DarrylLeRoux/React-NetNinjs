224239a317bc0cea20d54bec61ed41acda7989b2
Darryl Le Roux <devdarryl@outlook.com>
2022-08-01 21:59:32 +0200

    using .map withe keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("mario");
+  const [events, setEvents] = useState([
+    { title: "Marios birthday bash", id: 1 },
+    { title: "Bowers live stream", id: 2 },
+    { title: "Race on moo moo farm", id: 3 },
+  ]);
+
+  const handleNameChange = () => {
+    setName("luigi");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <button onClick={handleNameChange}>Change name</button>
+      {/* Render the lists on the page */}
+      {events.map((event) => (
+        // Put a key print on a parent element
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
